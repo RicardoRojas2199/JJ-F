260306e955936839b2ca87fbf4c24d407f4b3dd3
Rojas Arroyo <gerojas@indracompany.com>
2020-09-22 21:34:07 -0500

    feature: page animation

--- src/templates/project-template.js
+++ src/templates/project-template.js
@@ -1,53 +1,53 @@
 /**@jsx jsx */
-import { jsx } from 'theme-ui'
-import { graphql } from 'gatsby'
-import Img from 'gatsby-image'
-import { MDXRenderer } from 'gatsby-plugin-mdx'
+import { jsx } from "theme-ui";
+import { graphql } from "gatsby";
+import Img from "gatsby-image";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 
-import Layout from '../components/layout'
+import Layout from "../components/layout";
 
 const ProjectTemplate = ({ data }) => {
-  const { gallery } = data.mdx.frontmatter
+  const { gallery } = data.mdx.frontmatter;
   return (
     <Layout>
-      <div sx={{ borderBottom: '#ccc 1px solid', marginBottom: '1rem' }}>
+      <div sx={{ borderBottom: "#ccc 1px solid", marginBottom: "1rem" }}>
         <MDXRenderer>{data.mdx.body}</MDXRenderer>
       </div>
       <div
         sx={{
-          display: 'grid',
-          gridTemplateColumns: 'repeat(auto-fit, minmax(280px,1fr))',
-          gridGap: '16px',
-          gridAutoRows: '200px'
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fit, minmax(280px,1fr))",
+          gridGap: "16px",
+          gridAutoRows: "200px",
         }}
       >
-        {gallery.map(image => (
+        {gallery.map((image) => (
           <div
             key={image.childImageSharp.id}
             sx={{
               gridRow:
-                image.childImageSharp.fluid.aspectRatio > 0.7 && 'span 2',
+                image.childImageSharp.fluid.aspectRatio > 0.7 && "span 2",
               gridColumn:
-                image.childImageSharp.fluid.aspectRatio > 1.5 && 'span 2'
+                image.childImageSharp.fluid.aspectRatio > 1.5 && "span 2",
             }}
           >
             <Img
               fluid={image.childImageSharp.fluid}
               sx={{
-                borderRadius: '16px',
+                borderRadius: "16px",
                 boxShadow:
-                  '0px 2px 4px rgba(40, 41, 61, 0.5), 0px 8px 16px rgba(96, 97, 112, 0.3)'
+                  "0px 2px 4px rgba(40, 41, 61, 0.5), 0px 8px 16px rgba(96, 97, 112, 0.3)",
               }}
-              style={{ height: '100%', width: '100%' }}
+              style={{ height: "100%", width: "100%" }}
             />
           </div>
         ))}
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default ProjectTemplate
+export default ProjectTemplate;
 
 export const query = graphql`
   query($id: String!) {
@@ -66,4 +66,4 @@ export const query = graphql`
       }
     }
   }
-`
+`;
